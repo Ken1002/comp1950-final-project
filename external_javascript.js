bd5b8372077e7a9a1a560f17ba54e8e998270890
vickychern <Vikki@MacBook-Pro-Viktoria.local>
2017-03-25 10:55:40 -0700

    added new files

---
+++ external_javascript.js
@@ -0,0 +1,36 @@
+/*Victoria Chernyaeva COMP 1950*/
+$(document).ready(function () {
+
+	$("#nav").hide();
+    $("nav p").hide();
+	
+	
+	/*Open the menu*/
+	$("#nav_icon").click(function () {
+		$("#nav").slideToggle();
+		$("#nav_icon").fadeToggle();
+        $("nav p").slideToggle();
+	});
+	
+	/*Close the menu*/
+	$("nav p").click(function () {
+		$("#nav").slideToggle();
+		$("#nav_icon").fadeToggle();
+        $("nav p").fadeToggle();
+	});
+
+	/*Alertify plugin for the button on the about page*/
+    $(".button").on("click", function(event){
+    event.preventDefault();
+    alertify.confirm("Submit the feedback?", function(e){
+        if (e) {
+            $("#category-form").submit();
+            alertify.success("Your message was sent")
+            return true;
+        } else {
+            alertify.error("Your message was NOT sent");
+            return false;
+        }
+    });
+});
+});
