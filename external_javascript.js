f993845c6e7fc4c0fc5bc4f02ae5a8c91babeeea
vickychern <Vikki@MacBook-Pro-Viktoria.local>
2017-03-25 13:18:51 -0700

    enabled the plugin

--- external_javascript.js
+++ external_javascript.js
@@ -1,36 +1,19 @@
 /*Victoria Chernyaeva & Ken Andreasen COMP 1950 Final project*/
+/*Alertify plugin for the button on the about page*/
 $(document).ready(function () {
 
-	$("#nav").hide();
-    $("nav p").hide();
 	
-	
-	/*Open the menu*/
-	$("#nav_icon").click(function () {
-		$("#nav").slideToggle();
-		$("#nav_icon").fadeToggle();
-        $("nav p").slideToggle();
-	});
-	
-	/*Close the menu*/
-	$("nav p").click(function () {
-		$("#nav").slideToggle();
-		$("#nav_icon").fadeToggle();
-        $("nav p").fadeToggle();
-	});
-
-	/*Alertify plugin for the button on the about page*/
-    $(".button").on("click", function(event){
-    event.preventDefault();
-    alertify.confirm("Submit the feedback?", function(e){
-        if (e) {
-            $("#category-form").submit();
-            alertify.success("Your message was sent")
-            return true;
-        } else {
-            alertify.error("Your message was NOT sent");
-            return false;
-        }
-    });
+$(".button").on("click", function(event){
+event.preventDefault();
+alertify.confirm("Submit the quiz?", function(e){
+   if (e) {
+	  $("#category-form").submit();
+	  alertify.success("Your quiz was submitted")
+	  return true;
+   } else {
+	  alertify.error("Your quiz was NOT submitted");
+	  return false;
+   }
+});
 });
 });
